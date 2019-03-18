6af740aee908fe84164a5bf7cb52fd526bb7c467
Matan Anavi <MrBahur25@gmail.com>
2019-03-18 08:16:48 +0200

    Initial Commit

--- src/algorithms/test/RunSearchOnMaze.java
+++ src/algorithms/test/RunSearchOnMaze.java
@@ -8,15 +8,15 @@ import algorithms.search.*;
 import java.util.ArrayList;
 
 public class RunSearchOnMaze {
-//
-//    public static void main(String[] args) {
+
+    public static void main(String[] args) {
 //        IMazeGenerator mg = new MyMazeGenerator();
 //        Maze maze = mg.generate(30, 30);
 //        SearchableMaze searchableMaze = new SearchableMaze(/*maze*/);
 //        solveProblem(searchableMaze, new BreadthFirstSearch());
 //        solveProblem(searchableMaze, new DepthFirstSearch());
 //        solveProblem(searchableMaze, new BestFirstSearch());
-//    }
+    }
 //
 //    private static void solveProblem(ISearchable domain, ISearchingAlgorithm searcher) {
 //        //Solve a searching problem with a searcher
