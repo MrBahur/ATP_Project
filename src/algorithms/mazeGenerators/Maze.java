5d2ea7b42f9c9528d188f72719cfe30e31c085ee
Matan Anavi <MrBahur25@gmail.com>
2019-03-18 08:39:42 +0200

    added some functions to Maze

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -2,16 +2,41 @@ package algorithms.mazeGenerators;
 
 public class Maze {
 
+    private int[][] maze;
+    private Position startPosition;
+    private Position endPosition;
+
+    public Maze(int rows, int cols) {
+        maze = new int[rows][cols];
+    }
+
     //TODO implement this method, S for start, E for exit
-    public void print(){
+    public void print() {
 
     }
+
+
+    //<editor-fold desc="Getters">
     //TODO implement
-    public Position getStartPosition(){
+    public Position getStartPosition() {
         return null;
     }
+
     //TODO implement
-    public Position getGoalPosition(){
+
+    public Position getGoalPosition() {
         return null;
     }
+    //</editor-fold>
+
+
+    //<editor-fold desc="Setters">
+    public void setStartPosition(Position startPosition) {
+        this.startPosition = startPosition;
+    }
+
+    public void setEndPosition(Position endPosition) {
+        this.endPosition = endPosition;
+    }
+    //</editor-fold>
 }
