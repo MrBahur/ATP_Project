8799c1aa38fa676582e6d081e83e553e75c07b42
Matan Anavi <MrBahur25@gmail.com>
2019-04-02 19:31:52 +0300

    made some changes but we done

--- src/algorithms/search/BestFirstSearch.java
+++ src/algorithms/search/BestFirstSearch.java
@@ -5,7 +5,7 @@ import java.util.PriorityQueue;
 public class BestFirstSearch extends BreadthFirstSearch{
     public BestFirstSearch() {
         super("Best First Search");
-        this.queue = new PriorityQueue<AState>((AState o1, AState o2) -> (o1.getCost()<o2.getCost())?1:-1);//need to find new compareTo
+        this.queue = new PriorityQueue<AState>((AState o1, AState o2) -> Double.compare(o2.getCost(), o1.getCost()));//need to find new compareTo
 
 
     }
