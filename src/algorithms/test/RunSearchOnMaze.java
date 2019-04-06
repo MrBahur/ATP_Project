254cd58039d5fc41781d1a7f56f2945204bfca29
Matan Anavi <MrBahur25@gmail.com>
2019-04-06 10:22:32 +0300

    made some changes but we done

--- src/algorithms/test/RunSearchOnMaze.java
+++ src/algorithms/test/RunSearchOnMaze.java
@@ -13,7 +13,7 @@ public class RunSearchOnMaze {
 
     public static void main(String[] args) {
         IMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(10, 10);
+        Maze maze = mg.generate(30, 30);
         maze.print();
         SearchableMaze searchableMaze = new SearchableMaze(maze);
         solveProblem(searchableMaze, new BreadthFirstSearch());
@@ -23,9 +23,7 @@ public class RunSearchOnMaze {
 
     private static void solveProblem(ISearchable domain, ISearchingAlgorithm searcher) {
         //Solve a searching problem with a searcher
-        long startTime = System.currentTimeMillis();
         Solution solution = searcher.solve(domain);
-        System.out.println(System.currentTimeMillis() - startTime);
         System.out.println(String.format("'%s' algorithm - nodes evaluated: %s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
         //Printing Solution Path
         System.out.println("Solution path:");
