82cdf473439ac3bc4308c3d76521fed1dfb201a8
Matan Anavi <MrBahur25@gmail.com>
2019-04-28 16:01:26 +0300

    added necessary classes and methods for Server and Client and added the test file

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,4 +1,12 @@
 package Client;
 
+import java.net.InetAddress;
+
 public class Client {
+    public Client(InetAddress ip, int port, IClientStrategy s) {
+
+    }
+
+    public void communicateWithServer() {
+    }
 }
