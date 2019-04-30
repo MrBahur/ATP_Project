6582052b519a6959ef7d086fae4d5a9572cad654
Matan Anavi <MrBahur25@gmail.com>
2019-04-30 12:13:25 +0300

    fixed compressor again! because Daniel broke it again.

--- src/IO/MyCompressorOutputStream.java
+++ src/IO/MyCompressorOutputStream.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 public class MyCompressorOutputStream extends OutputStream {
 
     private OutputStream out;
-
     private HashMap<ArrayList<Byte>, Pair<Integer, Integer>> dictionary;
     /*pattern, pair<index,previous index>
              HashMap of:
@@ -21,11 +20,8 @@ public class MyCompressorOutputStream extends OutputStream {
             such pattern the index equals to 0. */
 
     private ArrayList<Pair<Integer, Byte>> resultPairs;
-            /*The final result  - pairs of the previous pattern index and the additional byte which
-            together create a new pattern. */
-
-    private ArrayList<Byte> newPattern;
-
+    /*The final result  - pairs of the previous pattern index and the additional byte which
+    together create a new pattern. */
     private int numOfBytesForIndex;
     private int previousPatternIndex;
     private int currentPatternIndex;
@@ -33,36 +29,36 @@ public class MyCompressorOutputStream extends OutputStream {
 
     public MyCompressorOutputStream(OutputStream out) {
         this.out = out;
-        dictionary = new HashMap();
-        resultPairs = new ArrayList<>();
-        currentPatternIndex = 1;
-        numOfBytesForIndex = 1;
+        dictionary = null;
+        resultPairs = null;
+        numOfBytesForIndex = 0;
+        previousPatternIndex = 0;
+        currentPatternIndex = 0;
 
     }
 
+
     @Override
     public void write(int b) throws IOException {
-
-        out.write((byte)b);
+        out.write((byte) b);
     }
 
     /**
      * Sets an Integer to be written to the output stream
-     * @param b The original value
+     *
+     * @param b                  The original value
      * @param numOfBytesPerIndex the number of bytes needed in order to write a representation
      *                           of the value
-     * @throws IOException
+     * @throws IOException because of unchecked call to write
      */
-    public void write(int b, int numOfBytesPerIndex) throws IOException {
+    private void write(int b, int numOfBytesPerIndex) throws IOException {
 
         if (numOfBytesPerIndex == 1) {
             write(b);
-        }
-        else if (numOfBytesPerIndex == 2) {
+        } else if (numOfBytesPerIndex == 2) {
             write(b / 255); //multiply by
             write(b % 255); //remainder
-        }
-        else if (numOfBytesPerIndex == 3) {
+        } else if (numOfBytesPerIndex == 3) {
             write(b / 65025); //65025 multiply by
             write((b % 65025) / 255); //first remainder multiply by
             write((b % 65025) % 255); //second remainder
@@ -70,38 +66,39 @@ public class MyCompressorOutputStream extends OutputStream {
         }
     }
 
+    /**
+     * Compresses the given array and writes the compressed array into this output stream
+     *
+     * @param b the given byte array
+     */
+
     @Override
-/**
- * Compresses the given array and writes the compressed array into this output stream
- */
-    public void write(byte[] b) throws IOException {
+    public void write(byte[] b) {
+        //initializing fields (every time we call this function)
+        dictionary = new HashMap<>();
+        resultPairs = new ArrayList<>();
+        currentPatternIndex = 1;
+        numOfBytesForIndex = 1;
+        ArrayList<Byte> newPattern = null;
 
         if (b == null) {
             throw new NullPointerException();
         } else {
-
-            dictionary.put(null, new Pair(0, 0)); //The first pattern (default)
+            dictionary.put(null, new Pair<Integer, Integer>(0, 0)); //The first pattern (default)
             int i = 0;
-
             while (i < b.length) {
-
                 previousPatternIndex = 0;
                 newPattern = new ArrayList<>();
                 newPattern.add(b[i]);
-
                 while (indexOf(dictionary, newPattern) != -1) { //The pattern is in the list
-
                     previousPatternIndex = indexOf(dictionary, newPattern);
-
                     if (i == b.length - 1) {
                         break;
                     }
                     i++;
                     newPattern.add(b[i]);
                 }
-
                 dictionary.put(newPattern, new Pair<>(currentPatternIndex, previousPatternIndex));
-
                 currentPatternIndex++;
                 if (currentPatternIndex > 255) {//For mazes of about 100*100 or bigger
                     numOfBytesForIndex = 2;
@@ -109,16 +106,16 @@ public class MyCompressorOutputStream extends OutputStream {
                 if (currentPatternIndex > 65025) {//For mazes of about 1300*1300 or bigger
                     numOfBytesForIndex = 3;
                 }
-
-                Pair newResultPair = new Pair(previousPatternIndex, b[i]);
-
+                Pair<Integer, Byte> newResultPair = new Pair<>(previousPatternIndex, b[i]);
                 resultPairs.add(newResultPair);
                 i++;
             }
-
-            writeToOutput(resultPairs, numOfBytesForIndex);
+            try {
+                writeToOutput(resultPairs, numOfBytesForIndex);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        return;
     }
 
     /**
@@ -129,7 +126,7 @@ public class MyCompressorOutputStream extends OutputStream {
      * @return if the pattern exists in the dictionary - returns its index in the dictionary.
      * else, returns -1
      */
-    public int indexOf(HashMap<ArrayList<Byte>, Pair<Integer, Integer>> dictionary, ArrayList<Byte> pattern) {
+    private int indexOf(HashMap<ArrayList<Byte>, Pair<Integer, Integer>> dictionary, ArrayList<Byte> pattern) {
 
 
         if (dictionary.get(pattern) == null) return -1;
@@ -137,9 +134,9 @@ public class MyCompressorOutputStream extends OutputStream {
         return dictionary.get(pattern).getKey();
     }
 
-    public void writeToOutput(ArrayList<Pair<Integer, Byte>> resultPairs, int numOfBytesPerIndex) throws IOException {
+    private void writeToOutput(ArrayList<Pair<Integer, Byte>> resultPairs, int numOfBytesPerIndex) throws IOException {
 
-        out.write(((Integer)numOfBytesPerIndex).byteValue());
+        out.write(((Integer) numOfBytesPerIndex).byteValue());
 
         for (int i = 0; i < resultPairs.size(); i++) {
 
