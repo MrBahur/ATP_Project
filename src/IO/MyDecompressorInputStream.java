bfd9d829d54ed948a123bf98f987810ef9aa7b8f
Matan Anavi <MrBahur25@gmail.com>
2019-04-30 12:18:47 +0300

    fixed decompress

--- src/IO/MyDecompressorInputStream.java
+++ src/IO/MyDecompressorInputStream.java
@@ -37,15 +37,16 @@ public class MyDecompressorInputStream extends InputStream {
 
     /**
      * Sets a byte[] using the information read from the input Stream
+     *
      * @param b The byte array to be set
-     * @return
-     * @throws IOException
+     * @return **daniel please write what it's returns**
+     * @throws IOException because of unchecked call to in.read
      */
     @Override
     public int read(byte[] b) throws IOException {
 
         //initializing fields (every time we call this function)
-        dictionary = new HashMap();
+        dictionary = new HashMap<>();
         ArrayList<Byte> newPattern = null;
         currentIndex = 1;
         numOfBytesPerIndex = 0;
@@ -67,55 +68,58 @@ public class MyDecompressorInputStream extends InputStream {
             if (previousIndex != 0) {
 
                 ArrayList<Byte> previousPattern = dictionary.get(previousIndex);
-
-                for (int i = 0; i < previousPattern.size(); i++) {
-                    newPattern.add(previousPattern.get(i));
-                }
+                newPattern.addAll(previousPattern);
             }
-            newPattern.add((byte)extraByte);
+            newPattern.add((byte) extraByte);
             dictionary.put(currentIndex, newPattern);
             //Add the pattern and the index that indicates its turn to be added to the array
 
             currentIndex++;
         }
 
-        setBytesArray(dictionary,b,currentIndex);
+        setBytesArray(dictionary, b, currentIndex);
 
         return 0;
     }
 
     /**
      * Calculates the value of the index using the information read from the input stream
+     *
      * @param numOfBytesPerIndex Indication of how many bytes are needed in order to get the value of the index
      * @return the value of the index
-     * @throws IOException
      */
-    public Integer previousIndex(Integer numOfBytesPerIndex) throws IOException{
+    private Integer previousIndex(Integer numOfBytesPerIndex) {
 
-        Integer previousIndex;
-        if (numOfBytesPerIndex == 1) {
-            previousIndex = in.read();
-        } else if (numOfBytesPerIndex == 2) {
-            int mul = in.read();
-            int remainder = in.read();
-            previousIndex = (255 * mul + remainder);
-        } else { //numOfBytesPerIndex==3
-            int firstMultiplication = in.read();
-            int secondMultiplication = in.read();
-            int remainder = in.read();
-            previousIndex = ((65025 * firstMultiplication) + (255 * secondMultiplication) + remainder);
+        Integer previousIndex = null;
+        try {
+
+            if (numOfBytesPerIndex == 1) {
+                previousIndex = in.read();
+            } else if (numOfBytesPerIndex == 2) {
+                int mul = in.read();
+                int remainder = in.read();
+                previousIndex = (255 * mul + remainder);
+            } else { //numOfBytesPerIndex==3
+                int firstMultiplication = in.read();
+                int secondMultiplication = in.read();
+                int remainder = in.read();
+                previousIndex = ((65025 * firstMultiplication) + (255 * secondMultiplication) + remainder);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return previousIndex;
     }
 
     /**
      * Sets the bytes Array
-     * @param dictionary Where all the patterns are kept (the key indicates their turn to be added to the final byte[])
-     * @param b The Array to be set
+     *
+     * @param dictionary   Where all the patterns are kept (the key indicates their turn to be added to the final byte[])
+     * @param b            The Array to be set
      * @param currentIndex The number of patterns that are kept in the dictionary
-     *                    (which is equal to the number of bytes in b)
+     *                     (which is equal to the number of bytes in b)
      */
-    public void setBytesArray(Map<Integer, ArrayList<Byte>> dictionary, byte[] b, int currentIndex){
+    private void setBytesArray(Map<Integer, ArrayList<Byte>> dictionary, byte[] b, int currentIndex) {
 
         int i = 0;
         for (int j = 1; j < currentIndex; j++) {
